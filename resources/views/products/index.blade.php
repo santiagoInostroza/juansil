fbe47c435c193570b4f01c6b916549e911e4f630
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-03-25 20:28:45 -0300

    cambio de prueba en producto

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,10 +1,12 @@
 <x-app-layout>
+    
     <div class="container max-w-7xl py-8">
         @if (count($productos)>0)
         @isset($name)
         <h2 class="font-bold text-gray-600 pt-8 pb-4 text-xl">Busquedas relacionadas con "{{$name}}"</h2>
+        
         @endisset
-
+hola
             <div class="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:max-w-5xl xl:max-w-7xl mx-auto" style="height: max-content">
                 @foreach ($productos as $producto)
                     <livewire:producto :producto='$producto' :key="$producto->id" />
