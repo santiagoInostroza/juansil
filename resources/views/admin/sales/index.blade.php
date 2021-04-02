4e35d68ef77b39bd9c3eaa22f862cdfac284133a
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-04-01 23:42:10 -0300

    se agrega total de ventas

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -37,7 +37,7 @@
         </script>
     @endif
 
-    <div class="fixed-bottom p-5 shadow text-center">Total ventas ${{number_format($sales->sum('total'),0,',','.')}}</div>
+    <div class="fixed-bottom p-2 bg-white shadow text-center">Total ventas ${{number_format($sales->sum('total'),0,',','.')}}</div>
     <div class="card">
         <div class="card-body">
             <table id="tablaVentas" class="table table-striped table-bordered" style="width:100%">
