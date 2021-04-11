85439d82fefdc97d85715d09f32fbdbb49387cf2
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-04-11 16:08:41 -0400

    varios pagos pendientes, clientes

--- resources/views/livewire/admin/customers/index.blade.php
+++ resources/views/livewire/admin/customers/index.blade.php
@@ -16,42 +16,45 @@
                </thead>
                <tbody>
                    @foreach ($customers as $customer)
-                  <tr>
-                    <td>
-                        <div class="d-flex">{{$customer->id}}</div>
-                    </td>    
-                    <td>
-                       <div class="h5 ">
-                           {{$customer->name}}
-                        </div>
-                        <div>
-                            {{$customer->direccion}} 
-                            @isset($customer->block) Torre {{$customer->block}} @endisset 
-                            @isset($customer->depto) depto {{$customer->depto}} @endisset
-                        </div>
-                    </td>    
-                    <td>
-                        <div>{{$customer->telefono}}</div>
-                        <div>{{$customer->celular}}</div>
-                    </td>
-                    <td></td>
-                    <td width='10px'>
-                        <div class="d-flex">
-                            <a href="{{ route('admin.customers.datos_cliente', $customer) }}" title="Ver datos del cliente" class="btn btn-secondary btn-sm"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.customers.edit', $customer) }}" class="btn btn-secondary btn-sm mx-2"><i class="fas fa-pen"></i></a>
-                            <form action="{{ route('admin.customers.destroy', $customer) }}" method='POST' class="alerta_eliminar">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="btn btn-danger btn-sm"><i class="far fa-trash-alt"></i></button>
-                            </form>
-                        </div>
-                      
-                    </td>
-                </tr>     
+                    <tr>
+                        <td>
+                            <div class="d-flex">{{$customer->id}}</div>
+                        </td>    
+                        <td>
+                        <div class="h5 ">
+                            {{$customer->name}}
+                            </div>
+                            <div>
+                                {{$customer->direccion}} 
+                                @isset($customer->block) Torre {{$customer->block}} @endisset 
+                                @isset($customer->depto) depto {{$customer->depto}} @endisset
+                            </div>
+                        </td>    
+                        <td>
+                            <div>{{$customer->telefono}}</div>
+                            <div>{{$customer->celular}}</div>
+                        </td>
+                        <td></td>
+                        <td width='10px'>
+                            <div class="d-flex">
+                                <a href="{{ route('admin.customers.datos_cliente', $customer) }}" title="Ver datos del cliente" class="btn btn-secondary btn-sm"><i class="fas fa-eye"></i></a>
+                                <a href="{{ route('admin.customers.edit', $customer) }}" class="btn btn-secondary btn-sm mx-2"><i class="fas fa-pen"></i></a>
+                                <form action="{{ route('admin.customers.destroy', $customer) }}" method='POST' class="alerta_eliminar">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn btn-danger btn-sm"><i class="far fa-trash-alt"></i></button>
+                                </form>
+                            </div>
+                        
+                        </td>
+                    </tr>     
                    @endforeach
                </tbody>
            </table>
-       </div>
+        </div>
+        <div class="card-footer">
+            {{ $users->links() }}
+        </div>
 
    </div>
 </div>
