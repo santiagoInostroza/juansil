3420e46834948d71db0415802894e6add17c93e6
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-04-16 00:50:20 -0400

    ajustes

--- app/Http/Livewire/Admin/Ventas/Lista.php
+++ app/Http/Livewire/Admin/Ventas/Lista.php
@@ -29,9 +29,9 @@ class Lista extends Component{
 
     public function render(){
         if( $this->search == 'solo despachos') {
-            $ventas = Sale::where('delivery',1)->paginate(100);
+            $ventas = Sale::where('delivery',1)->paginate(50);
         }elseif($this->search == 'solo bodega'){
-            $ventas = Sale::where('delivery','!=',1)->paginate(100);
+            $ventas = Sale::where('delivery','!=',1)->paginate(50);
         }else{
             $ventas = Sale::join('customers','sales.customer_id','=','customers.id')
             ->where('customers.name','like','%'. $this->search . '%')
@@ -42,7 +42,7 @@ class Lista extends Component{
             ->orWhere('sales.id','like','%'. $this->search . '%')
             ->select('sales.*')
             ->orderBy($this->sort,$this->direction)
-            ->paginate(100);
+            ->paginate(50);
         }
 
         $this->diferencia = 0;
