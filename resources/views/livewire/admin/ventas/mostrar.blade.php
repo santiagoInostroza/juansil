94ae182922b29d90627f5791c32aee79be8b830e
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-04-16 00:19:15 -0400

    ajustes

--- resources/views/livewire/admin/ventas/mostrar.blade.php
+++ resources/views/livewire/admin/ventas/mostrar.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div wire:click="$set('open',true)" class="btn btn-secondary btn-sm mr-2"><i class="far fa-eye"></i></div>
     @if ($open)
-        <div class="position-fixed bg-secondary" style="top:0;bottom:0;left:0;right:0;opacity: 0.5"></div>
+        <div class="position-fixed bg-secondary" style="top:0;bottom:0;left:0;right:0;opacity: 0.5;z-index: 9998"></div>
         
         
-        <div class="position-fixed bg-white"  style="top:0;left:0;right:0; border-radius:5px" >
+        <div class="position-fixed bg-white m-5"  style="top:0;left:0;right:0; border-radius:5px;z-index: 9999" >
             <div wire:click="$set('open',false)" class="p-3 btn" style="float: right"><i class="fas fa-times"></i></div>
             <h3 class="p-4">Detalle de venta</h3>
             <h4 class="px-4">{{$venta->customer->name}} {{date("d-m-Y",strtotime($venta->date))}}</h4>
