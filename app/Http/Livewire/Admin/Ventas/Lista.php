810318500366739cdcbd1a440b8587fd4160199c
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-04-16 00:55:39 -0400

    ajustes

--- app/Http/Livewire/Admin/Ventas/Lista.php
+++ app/Http/Livewire/Admin/Ventas/Lista.php
@@ -42,7 +42,7 @@ class Lista extends Component{
             ->orWhere('sales.id','like','%'. $this->search . '%')
             ->select('sales.*')
             ->orderBy($this->sort,$this->direction)
-            ->paginate(50);
+            ->paginate(300);
         }
 
         $this->diferencia = 0;
