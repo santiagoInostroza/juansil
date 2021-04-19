fa65103c579d9c27b08849616767255818010e50
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-04-19 16:47:44 -0400

    ajustes

--- resources/views/livewire/admin/resumen.blade.php
+++ resources/views/livewire/admin/resumen.blade.php
@@ -41,4 +41,66 @@
             ${{ number_format($total_bodega, 0, ',', '.') }}
         </div>
     </div>
+    <div class="card-body">
+        <div class="grid grid-cols-12">
+            <div>Nombre</div>
+            <div>fecha</div>
+            <div class="col-span-6">Detalle</div>
+            <div class="col-span-4 grid grid-cols-3">
+                <div>Costo</div>
+                <div>Venta</div>
+                <div>Diferencia</div>
+            </div>
+        </div>
+        @foreach ($sales as $sale)
+            <div class="border p-2 grid grid-cols-12">
+               
+                <div class="">
+                    {{$sale->customer->name}}
+                </div>
+                <div class="">
+                    {{date("d-m-Y",strtotime($sale->date))}}
+                </div>
+               
+                <div class="col-span-6">
+                  
+                @foreach ($sale->sale_items as $item)
+
+                    <div class="flex justify-between pb-2">
+                        <div>
+                            {{$item->cantidad_total}}  {{$item->product->name}} x  {{$item->cantidad_por_caja}}
+                        </div>
+                        <div>
+                            {{$item->cantidad_total}} un. a ${{number_format($item->precio,0,',','.')}} c/u  ${{number_format($item->precio_total,0,',','.')}}
+                        </div>
+                        <div>
+                            @foreach ($item->product->purchasePrices as $precio)
+                                a ${{number_format($precio->precio,0,',','.')}} =  ${{number_format($item->cantidad_total * $precio->precio,0,',','.')}}
+                            @endforeach
+                        </div>
+                        <div>
+
+                        </div>
+                     
+                    </div>
+                    <div>
+                       
+                     
+                    </div>
+                @endforeach
+                </div>
+                <div class="col-span-4 grid grid-cols-3">
+                    <div class="">
+                        ${{number_format($sale->total,0,',','.')}}
+                    </div>
+                    <div class="">
+                        ${{number_format($sale->total,0,',','.')}}
+                    </div>
+                    <div class="">
+                        ${{number_format($sale->total,0,',','.')}}
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
