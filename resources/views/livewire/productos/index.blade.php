4d7ec42d93a8e8072336f542e32b059073c84011
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-07-22 01:05:47 -0400

    ajuste

--- resources/views/livewire/productos/index.blade.php
+++ resources/views/livewire/productos/index.blade.php
@@ -7,7 +7,7 @@
        <h2 class="w-full p-4 text-3xl bg-gray-200 text-gray-800 font-bold text-center cursor-pointer uppercase font-mono">Ver lista con todos los productos</h2>
     </a>
    <hr>
-   explicacion del sistema de compras breve
+   breve explicacion del sistema de compras
    <hr>
    ofertas
    <hr>
@@ -16,6 +16,8 @@
    <hr>
    lista x más VENDIDOS
    <hr>
+   Lo ultimo que llegó
+
 
  
 </div>
