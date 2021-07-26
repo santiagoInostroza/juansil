34f0b53ebdc46bb002c905b53d575e8a775184db
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-07-25 21:24:06 -0400

    ajuste

--- resources/views/livewire/buscador-productos.blade.php
+++ resources/views/livewire/buscador-productos.blade.php
@@ -111,23 +111,9 @@
                                             <div class="cursor-default inline-flex items-center px-4 py-2 bg-gray-100 border border-gray-300 rounded-md font-semibold text-xs text-gray-300 uppercase tracking-widest shadow-sm  focus:outline-none    transition ease-in-out duration-150" disabled> Agotado</div>
                                         </div>
                                     @endif
-
-
-
-
-
-
-
-
-
-
-                                       
-                                   
                                 </div>
                             </div>
                             <hr>
-                        
-                            
                         @endforeach
                                      
                     @endif
--- resources/views/livewire/productos/show.blade.php
+++ resources/views/livewire/productos/show.blade.php
@@ -1,6 +1,6 @@
     <div class="container py-11 lg:max-w-7xl ">
-        <div class="mx-6">
-            <div class="grid grid-cols md:grid-cols-2 gap-4">
+        <div class="mx-6 mb-6" >
+            <div class="grid grid-cols md:grid-cols-2 gap-4 mb-6">
                 <figure>
                     @isset($producto->image->url)
                             <img class="object-contain h-52 sm:h-96 w-full object-center"
@@ -8,8 +8,8 @@
                     @endisset
                 </figure>
 
-                <div class="">
-                    <div class="">
+                <div class="flex flex-col justify-center">
+                    <div class="mb-2">
                         @foreach ($producto->tags as $tag)
                             <a href="{{route('products.tag',$tag)}}">
                                 <span
@@ -19,60 +19,84 @@
                         @endforeach
                     </div>
                     <h1 class="text-xl text-gray-600">{{ $producto->name }}</h1>
-                    <div class="font-bold py-2">
+                    <div class="text-xl font-bold pb-2">
                         {{ $producto->brand->name }}
                     </div>
+
+                    @foreach ($producto->salePrices as $precio)
+               
+                        <div class="mb-4">
+                            <div class="text-lg w-max-content">
+                                <div>
+                                    Desde {{$precio->quantity}} un. a 
+                                    <div class="font-bold text-gray-500 text-3xl">
+                                        ${{number_format($precio->price,0,',','.')}} c/u
+                                    </div>
+                                </div>
+                            </div>
+                            @if ($precio->quantity>1)
+                            <div class="text-base w-max-content bg-red-100 rounded-xl px-2">
+                                    Valor al llevar las {{$precio->quantity}} un. 
+                                    <span class="text-red-500 text-xl">
+                                        ${{number_format($precio->total_price,0,',','.')}}
+                                    </span>
+                                </div>
+                                @endif
+                                
+                        </div>
+                        
+                    @endforeach
                 </div>
             </div>
-            <br><hr><br>
+            <hr>
+            <div class="fixed left-0 bottom-0 py-6 bg-white text-center w-full border-t mr-6 z-10 ">
+                @if ($producto->stock>0)
+                    <div class=" flex flex-col text-xs">
+                        @if (session()->has('carrito.'.$producto->id))
+                          
+                                <div class="flex justify-center gap-2">
+                                    <input type="number" min="1" class="p-1 w-12 text-center text-gray-500 text-xl font-bold cantidad_producto_{{$producto->id}}" value="{{ (isset(session('carrito')[$producto->id])) ? session('carrito')[$producto->id]['cantidad']:'1' }}"
+                                        wire:ignore 
+                                        onchange="return buscadorSetCantidad({{ $producto->id }}, {{$producto->stock}});"  
+                                        id='cantidad_producto_{{$producto->id}}'  
+                                        data-pid="{{$producto->id}}"
+                                    > 
+                                
+                                    <x-jet-secondary-button onclick="return buscadorDisminuyeCantidad({{ $producto->id }});" data-pid="{{$producto->id}}">-</x-jet-secondary-button>
+                                    <x-jet-secondary-button onclick="return buscadorAumentaCantidad({{ $producto->id }}, {{$producto->stock}});"  data-pid="{{$producto->id}}">+</x-jet-secondary-button>
+                                </div>
+                           
+                        @else
+                            <div>
+                                <x-jet-secondary-button wire:click="addToCart({{$producto->id}})" class="" >
+                                    <i class="fas fa-cart-plus m-1"></i> Agregar al carrito
+                                </x-jet-secondary-button>
 
-            {{-- PRECIOS --}}
-
-            @foreach ($producto->salePrices as $precio)
-               
-                <div class="text-base w-max-content">
-                    <div>
-                        Desde {{$precio->quantity}} un. a 
-                        <div class="font-bold text-gray-500 text-3xl">
-                            ${{number_format($precio->price,0,',','.')}} c/u
-                        </div>
+                            </div>
+                        @endif
                     </div>
-                </div>
-                @if ($precio->quantity>1)
-                    <div class="text-base w-max-content bg-red-100 rounded-xl px-2">
-                        Valor al llevar las {{$precio->quantity}} un. 
-                        <span class="text-red-500 text-xl">
-                            ${{number_format($precio->total_price,0,',','.')}}
-                        </span>
+                @else
+                    <div class="text-center text-red-500 text-xl font-bold">
+                        Este producto se encuentra sin stock!!
                     </div>
                 @endif
                 
-            @endforeach
-
-            <br><br>
-                <hr>
+            </div>
 
             @isset($producto->description)
-                <h2 class="text-2xl my-4 py-2 ">Descripcion del producto</h2>
-                <div class="">
+                <div class="my-6">
+                    <h2 class="text-2xl my-4 py-2 ">Descripcion del producto</h2>
+                    <div class="">
                         {!!$producto->description!!}
+                    </div>
                 </div>
             @endisset
+            <hr>
         </div>
 
+       
 
-            
-            
-    
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <hr>
-        <aside class="md:col-span-2 lg:col-span-3 xl:col-span-1 ">
+        <aside class="md:col-span-2 lg:col-span-3 xl:col-span-1 m-6">
             <div>
                 <h1 class="text-2xl font-bold text-gray-600 mb-4">Más de {{ $producto->category->name }}</h1>
                 <ul class="flex flex-wrap justify-around ">
@@ -102,6 +126,8 @@
             </div>
 
         </aside>
+
+
 
 
 
