84c99d8522ad5614aa801b474085118cbf1f188d
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-07-25 22:10:41 -0400

    ajuste

--- resources/views/livewire/cart/index.blade.php
+++ resources/views/livewire/cart/index.blade.php
@@ -25,7 +25,7 @@
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
             </button>
         </div>
-        <div class="card fixed z-40 top-0 w-screen sm:w-max-content right-0 h-screen hidden" :class="{hidden: !animate}"  {{--@click.away="animate = false"--}} 
+        <div class="card fixed z-50 top-0 w-screen sm:w-max-content right-0 h-screen hidden" :class="{hidden: !animate}"  {{--@click.away="animate = false"--}} 
             x-show="animate" 
             x-transition:enter="transition ease-out duration-1000" 
             x-transition:enter-start="opacity-0 transform scale-90"  
