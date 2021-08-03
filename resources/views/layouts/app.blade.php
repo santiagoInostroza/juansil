6934157f58fcc6cd84ebdea61657ce3cb6d7bf79
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-08-03 19:31:36 -0400

    ajuste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,39 +32,7 @@
 
 
 
-        {{-- ESTILOS DE CARRUSEL --}}
-        <style>
-			.carousel-open:checked + .carousel-item {
-				position: static;
-				opacity: 100;
-			}
-			.carousel-item {
-				-webkit-transition: opacity 0.6s ease-out;
-				transition: opacity 0.6s ease-out;
-			}
-			#carousel-1:checked ~ .control-1,
-			#carousel-2:checked ~ .control-2,
-			#carousel-3:checked ~ .control-3 {
-				display: block;
-			}
-			.carousel-indicators {
-				list-style: none;
-				margin: 0;
-				padding: 0;
-				position: absolute;
-				bottom: 2%;
-				left: 0;
-				right: 0;
-				text-align: center;
-				z-index: 9;
-			}
-			#carousel-1:checked ~ .control-1 ~ .carousel-indicators li:nth-child(1) .carousel-bullet,
-			#carousel-2:checked ~ .control-2 ~ .carousel-indicators li:nth-child(2) .carousel-bullet,
-			#carousel-3:checked ~ .control-3 ~ .carousel-indicators li:nth-child(3) .carousel-bullet {
-				color: #2b6cb0;  /*Set to match the Tailwind colour you want the active one to be */
-			}
-		</style>
-
+  
 
 
 
@@ -138,8 +106,6 @@
         @stack('modals')
         @stack('js')
         @livewireScripts
-
-        {{-- ALERTAS --}}
       
    
 
@@ -147,7 +113,7 @@
             {{$js}}
         @endisset
 
-        <!-- at the end of <body> -->
-<script src="https://unpkg.com/@markusantonwolf/ta-gallery@latest/dist/js/ta-gallery.min.js"></script>
+       {{-- TA GALERY --}}
+    <script src="https://unpkg.com/@markusantonwolf/ta-gallery@latest/dist/js/ta-gallery.min.js"></script>
     </body>
 </html>
--- resources/views/livewire/productos/index.blade.php
+++ resources/views/livewire/productos/index.blade.php
@@ -68,54 +68,6 @@
 </div>
   
 
-    {{-- CARRUSEL --}}
-    <div class="relative bg-white shadow-2xl carousel">
-      <div class="relative w-full overflow-hidden carousel-inner">
-        <!--Slide 1-->
-         <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-         <div class="absolute opacity-0 carousel-item" style="height:50vh;">
-            <div class="">
-               <img class="object-fill w-screen" src="{{url('images/portada/Juansil1.png')}}" alt="">  
-            </div>
-         </div>
-         <label for="carousel-3" class="absolute inset-y-0 left-0 hidden w-10 h-10 my-auto ml-2 text-3xl font-bold leading-tight text-center text-black bg-white rounded-full cursor-pointer prev control-1 md:ml-10 hover:text-white hover:bg-blue-700" style="z-index: 9">‹</label>
-         <label for="carousel-2" class="absolute inset-y-0 right-0 hidden w-10 h-10 my-auto mr-2 text-3xl font-bold leading-tight text-center text-black bg-white rounded-full cursor-pointer next control-1 md:mr-10 hover:text-white hover:bg-blue-700" style="z-index: 9">›</label>
-         
-         <!--Slide 2-->
-         <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-         <div class="absolute opacity-0 carousel-item" style="height:50vh;">
-            <div class="block w-full h-full text-5xl text-center text-white bg-orange-500">
-               <img class="object-cover w-screen" src="{{url('images/portada/Juansil2.png')}}" alt=""> 
-            </div>
-         </div>
-         <label for="carousel-1" class="absolute inset-y-0 left-0 hidden w-10 h-10 my-auto ml-2 text-3xl font-bold leading-tight text-center text-black bg-white rounded-full cursor-pointer prev control-2 md:ml-10 hover:text-white hover:bg-blue-700" style="z-index: 9">‹</label>
-         <label for="carousel-3" class="absolute inset-y-0 right-0 hidden w-10 h-10 my-auto mr-2 text-3xl font-bold leading-tight text-center text-black bg-white rounded-full cursor-pointer next control-2 md:mr-10 hover:text-white hover:bg-blue-700" style="z-index: 9">›</label> 
-         
-         <!--Slide 3-->
-         <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-         <div class="absolute opacity-0 carousel-item" style="height:50vh;">
-            <div class="block w-full h-full text-5xl text-center text-white bg-green-500">Slide 3</div>
-         </div>
-         <label for="carousel-2" class="absolute inset-y-0 left-0 hidden w-10 h-10 my-auto ml-2 text-3xl font-bold leading-tight text-center text-black bg-white rounded-full cursor-pointer prev control-3 md:ml-10 hover:text-white hover:bg-blue-700" style="z-index: 9">‹</label>
-         <label for="carousel-1" class="absolute inset-y-0 right-0 hidden w-10 h-10 my-auto mr-2 text-3xl font-bold leading-tight text-center text-black bg-white rounded-full cursor-pointer next control-3 md:mr-10 hover:text-white hover:bg-blue-700" style="z-index: 9">›</label>
-   
-         <!-- Add additional indicators for each slide-->
-         <ol class="carousel-indicators">
-            <li class="inline-block mr-3">
-               <label for="carousel-1" class="block text-4xl text-white cursor-pointer carousel-bullet hover:text-blue-700">•</label>
-            </li>
-            <li class="inline-block mr-3">
-               <label for="carousel-2" class="block text-4xl text-white cursor-pointer carousel-bullet hover:text-blue-700">•</label>
-            </li>
-            <li class="inline-block mr-3">
-               <label for="carousel-3" class="block text-4xl text-white cursor-pointer carousel-bullet hover:text-blue-700">•</label>
-            </li>
-         </ol>
-         
-      </div>
-   </div>
-   
-   <hr>
 
    
 <div class="flex justify-start bg-gradient-to-r from-white to-gray-200">
