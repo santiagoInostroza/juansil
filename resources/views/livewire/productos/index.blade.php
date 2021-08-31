66dce5c08a7c8c9cd24ffbb88b98d238b2fc8a3e
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-08-31 14:07:53 -0400

    ajuste

--- resources/views/livewire/productos/index.blade.php
+++ resources/views/livewire/productos/index.blade.php
@@ -360,31 +360,7 @@
       </div>
 
       
-         
-      <div class="flex justify-end bg-gradient-to-r from-white to-gray-200  pb-20  mb-10 sm:p-20">
-         <div class="px-20 my-20" >
-            <h2 class="text-3xl font-hairline text-red-500 sm:text-5xl">Explora nuestros <span class="inline-block mb-5 -mt-5 font-sans text-5xl font-bold sm:mb-0 sm:mt-0">productos</span></h2>
-            <p class="max-w-md text-gray-600">
-               <span class="font-extrabold"></span>Somos un joven emprendimiento enfocados en hacer crecer nuestra variedad de productos para que no te falte nada.
-            </p>
-            
-         
-            {{-- <div class="flex items-center justify-between max-w-xs gap-1 p-2 my-8 sm:gap-6 sm:max-w-md sm:w-max-content">
-               <a href="{{route('products.lista')}}">
-                  <img class="object-cover w-20 h-20 bg-gray-800 rounded-full sm:h-32 sm:w-32 " src="{{url('images/min/leche_min.jpg')}}" alt="">
-                  <p class="p-2 text-sm font-bold text-center text-gray-600" >Lacteos</p>
-               </a>
-               <a href="{{route('products.lista')}}">
-                  <img class="object-cover w-20 h-20 bg-gray-800 rounded-full sm:h-32 sm:w-32 " src="{{url('images/min/abarrotes3_min.jpeg')}}" alt="">
-                  <p class="p-2 text-sm font-bold text-center text-gray-600" >Abarrotes</p>
-               </a>
-               <a href="{{route('products.lista')}}">
-                  <img class="object-cover w-20 h-20 bg-gray-800 rounded-full sm:h-32 sm:w-32 " src="{{url('images/min/abarrotes_min.jpg')}}" alt="">
-                  <p class="p-2 text-sm font-bold text-center text-gray-600" >Todo</p>         
-               </a>
-            </div> --}}
-         </div>
-      </div>
+   
    
 
       {{-- PRODUCTOS POR CATEGORIA --}}
@@ -498,6 +474,32 @@
                </div>
             @endif
          @endforeach
+      </div>
+
+            
+      <div class="flex justify-end bg-gradient-to-r from-white to-gray-200  pb-20  mb-10 sm:p-20">
+         <div class="px-20 my-20" >
+            <h2 class="text-3xl font-hairline text-red-500 sm:text-5xl">Explora nuestros <span class="inline-block mb-5 -mt-5 font-sans text-5xl font-bold sm:mb-0 sm:mt-0">productos</span></h2>
+            <p class="max-w-md text-gray-600">
+               <span class="font-extrabold"></span>Somos un joven emprendimiento enfocados en hacer crecer nuestra variedad de productos para que no te falte nada.
+            </p>
+            
+         
+            {{-- <div class="flex items-center justify-between max-w-xs gap-1 p-2 my-8 sm:gap-6 sm:max-w-md sm:w-max-content">
+               <a href="{{route('products.lista')}}">
+                  <img class="object-cover w-20 h-20 bg-gray-800 rounded-full sm:h-32 sm:w-32 " src="{{url('images/min/leche_min.jpg')}}" alt="">
+                  <p class="p-2 text-sm font-bold text-center text-gray-600" >Lacteos</p>
+               </a>
+               <a href="{{route('products.lista')}}">
+                  <img class="object-cover w-20 h-20 bg-gray-800 rounded-full sm:h-32 sm:w-32 " src="{{url('images/min/abarrotes3_min.jpeg')}}" alt="">
+                  <p class="p-2 text-sm font-bold text-center text-gray-600" >Abarrotes</p>
+               </a>
+               <a href="{{route('products.lista')}}">
+                  <img class="object-cover w-20 h-20 bg-gray-800 rounded-full sm:h-32 sm:w-32 " src="{{url('images/min/abarrotes_min.jpg')}}" alt="">
+                  <p class="p-2 text-sm font-bold text-center text-gray-600" >Todo</p>         
+               </a>
+            </div> --}}
+         </div>
       </div>
 
 
