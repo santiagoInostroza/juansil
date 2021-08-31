54ed362e3cf12a49f21f8a4470fc5992e3a7a57a
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-08-30 23:57:36 -0400

    ajuste

--- resources/views/admin/customers/lista.blade.php
+++ resources/views/admin/customers/lista.blade.php
@@ -24,7 +24,7 @@
                             <div>{{$customer->user_id}} </div>
                         </div>
                        
-                    @elseif ($users->contains($customer->email))
+                    @elseif ($users->contains(Str::lower($customer->email)))
                         <div class="bg-red-600 text-white p-2">
                             Vincular cliente con usuario
                         </div>
