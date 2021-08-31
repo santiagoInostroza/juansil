c4daea887e996dc5159c17754322b42bdffd1c75
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-08-31 10:48:37 -0400

    ajuste

--- resources/views/components/dropdowns/dropdown.blade.php
+++ resources/views/components/dropdowns/dropdown.blade.php
@@ -31,7 +31,7 @@
                 <div  @click="onOptionClick(index)" :class="classOption(item.id, index)" :aria-selected="focusedOptionIndex === index" :id="'{{$id}}_opcion_' + index" x-ref="item.name" >       
                   <template x-if="item.image">
                       <figure>
-                        @if ( Storage::exists('products_thumb/${item.image.url}' ))
+                        @if ( Storage::exists('products_thumb/item.image.url' ))
                           <img class="w-10 h-10 rounded-full mr-4 object-contain" :src="`/storage/products_thumb/${item.image.url}`" />
                         @else
                           <img class="w-10 h-10 rounded-full mr-4 object-contain" :src="`/storage/${item.image.url}`" />
