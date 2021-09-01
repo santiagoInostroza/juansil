8a699acd992ce2ee4233b0ec186a563167df1ff5
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-01 17:23:26 -0400

    ajuste

--- resources/views/livewire/admin/products/index.blade.php
+++ resources/views/livewire/admin/products/index.blade.php
@@ -52,7 +52,7 @@
 
     <div class="p-4 relative border rounded">
         <x-jet-button wire:click="optimizarImagenes">Optimizar imagen</x-jet-button>
-        <div wire:loading>
+        <div wire:loading wire:target="optimizarImagenes">
             <x-spinner.spinner2></x-spinner.spinner2>
         </div>
     </div>
@@ -185,7 +185,7 @@
                                                     </div>
                                                     <div class="p-4 relative border rounded">
                                                         <x-jet-button wire:click="optimizarImagen({{ $product->id}})">Optimizar imagen</x-jet-button>
-                                                        <div wire:loading>
+                                                        <div wire:loading wire:target="optimizarImagen({{ $product->id}})">
                                                             <x-spinner.spinner2></x-spinner.spinner2>
                                                         </div>
                                                     </div>
