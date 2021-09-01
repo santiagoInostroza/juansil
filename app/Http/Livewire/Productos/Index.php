b6cb6bea30a908460a83c3b94d53997d19f806a5
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-01 10:45:24 -0400

    ajuste

--- app/Http/Livewire/Productos/Index.php
+++ app/Http/Livewire/Productos/Index.php
@@ -24,7 +24,7 @@ class Index extends Component{
             if ($this->onlyStock) {
                 $query->where('stock','>',0);
             }
-            $query->take(10);
+            $query->limit(10);
         }])
         ->where('id','!=', 3)->get();
 
