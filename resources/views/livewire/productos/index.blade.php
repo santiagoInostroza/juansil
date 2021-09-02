46647297afa4bf822b735b4ed170acc6077fa32b
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-02 19:08:35 -0400

    ajuste

--- resources/views/livewire/productos/index.blade.php
+++ resources/views/livewire/productos/index.blade.php
@@ -126,7 +126,7 @@
       {{-- TENTACIONES --}}
       @if ( $tentaciones )
       <div class="px-5 sm:px-20 mt-10" >
-         <h2 class="text-3xl font-hairline text-red-500 sm:text-5xl">  <span class="inline-block mb-5 -mt-5 font-sans text-5xl font-bold sm:mb-0 sm:mt-0"> Ten</span>taciones</h2>
+         <h2 class="text-3xl font-hairline text-red-500 sm:text-5xl">  <span class="inline-block mb-5 -mt-5 font-sans text-5xl font-bold sm:mb-0 sm:mt-0"> </span>Tentaciones</h2>
       </div>
       <div class="mb-20">
         
