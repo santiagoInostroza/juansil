d1936626d21e67293908865bb958fb4429dc4c90
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-03 02:30:20 -0400

    ajuste

--- resources/views/livewire/admin/reports/index.blade.php
+++ resources/views/livewire/admin/reports/index.blade.php
@@ -324,7 +324,7 @@
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="flex items-center gap-2">
                                                 <figure>
-                                                    <img class="h-16 w-16 object-cover" src="{{ Storage::url('products_thumb/' . $item->product->image->url) }}" alt="">
+                                                    <img class="h-20 w-full object-contain" src="{{ Storage::url('products_thumb/' . $item->product->image->url) }}" alt="{{$item->product->name}}">
                                                 </figure>
                                             
                                                 <div class="text-sm font-medium text-gray-900">
