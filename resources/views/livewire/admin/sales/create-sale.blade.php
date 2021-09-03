39deb2c34e4d35ff220a74d5e6ff0328582a00ee
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-03 02:14:50 -0400

    ajuste

--- resources/views/livewire/admin/sales/create-sale.blade.php
+++ resources/views/livewire/admin/sales/create-sale.blade.php
@@ -1,7 +1,7 @@
 <div>
 
 
-    <div  x-data='main()' @keyup.shift.c.window="$wire.open_add_item()">
+    <div  x-data='main()'>
         @if ($editSale)
             <x-jet-button>
                 <i class="fas fa-pen"></i>
@@ -14,7 +14,7 @@
 
 
 
-        <div  class="hidden" :class="{'hidden': !open}" >
+        <div  class="hidden" :class="{'hidden': !open}"   @keyup.shift.c.window="$wire.open_add_item()">
             <div class="bg-gray-500 w-full h-full fixed top-0 right-0 left-0 bottom-0 opacity-50 z-10 "></div>     
             <div class="absolute bg-white rounded-lg shadow-xl top-0 right-0 left-0 z-10 mt-4 mx-auto max-w-4xl">
                 <div class="px-6 pt-4">
