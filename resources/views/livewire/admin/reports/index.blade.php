eadad58b4df9f1ba119e67e3f44a930626649877
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-03 02:47:11 -0400

    ajuste

--- resources/views/livewire/admin/reports/index.blade.php
+++ resources/views/livewire/admin/reports/index.blade.php
@@ -322,7 +322,7 @@
                                             <div>{{$item->id}}</div>
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="flex items-center gap-2 w-max-content">
+                                            <div class="flex items-center gap-2">
                                                 <figure class="w-max-content">
                                                     <img class="h-24 w-full object-contain transform hover:scale-150" src="{{ Storage::url('products_thumb/' . $item->product->image->url) }}" alt="{{$item->product->name}}">
                                                 </figure>
