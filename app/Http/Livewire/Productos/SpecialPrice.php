69e0a17ff880fcae7e6be3b09b7da80999c530d1
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-14 21:39:33 -0300

    ajuste

--- app/Http/Livewire/Productos/SpecialPrice.php
+++ app/Http/Livewire/Productos/SpecialPrice.php
@@ -24,14 +24,14 @@ class SpecialPrice extends Component{
 
     public function render(){
 
-        if (Auth::user() && Auth::user()->id == 6) {
-            session()->forget('carritoSpecial');
+        // if (Auth::user() && Auth::user()->id == 6) {
+            session()->forget();
             $this->dispatchBrowserEvent('alerta_timer', [
                 'icon' => 'warning',
                 'msj' => "Se borro productos del carrito",
             ]);
 
-        }
+        // }
 
 
         $query = Product::where('name','like','%' . $this->search . '%')->where('special_sale_price','>',0);
