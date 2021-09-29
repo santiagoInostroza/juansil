cd05df1c3e429dc81862ac9aee5d7dc707597d30
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-29 19:17:24 -0300

    ajuste

--- resources/views/products/orders.blade.php
+++ resources/views/products/orders.blade.php
@@ -1,17 +1,10 @@
-@can('products.specialPrice')
+
     @extends('layouts.simple')
     @section('content')
 
 
-    @livewire('productos.orders')   
-@endsection
-
-@else
-    <x-app-layout>
         @livewire('productos.orders')   
-    </x-app-layout>
-  
-@endcan   
+    @endsection
 
 
 
