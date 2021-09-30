70d6750c8fd07d0e467eaff08f49c967f2f1e4df
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-30 00:26:54 -0300

    ajuste

--- resources/views/livewire/admin/sales/index.blade.php
+++ resources/views/livewire/admin/sales/index.blade.php
@@ -16,27 +16,32 @@
         </div>
     </div>
 
-    <div class="grid grid-cols-3 gap-4 w-max-content">
-        <div> Ventas totales {{ $sales->count() }} </div>
-        <div> Total  ${{ number_format($sales->sum('total'),0,',','.') }}</div>
-        <div> Diferencia  ${{ number_format(  $sales->sum('total') - $sales->sum('total_cost'),0,',','.') }}</div>
-        
-        <div> Ventas pagadas {{ $sales->where('payment_status',3)->count() }} </div>
-        <div> Total  ${{ number_format($sales->where('payment_status',3)->sum('total'),0,',','.') }}</div>
-        <div> Diferencia  ${{ number_format( $sales->where('payment_status',3)->sum('total') - $sales->where('payment_status',3)->sum('total_cost'),0,',','.') }}</div>
-        
-        <div> Ventas pendientes {{ $sales->where('payment_status',1)->count() }} </div>
-        <div> Total  ${{ number_format($sales->where('payment_status',1)->sum('total'),0,',','.') }}</div>
-        <div> Diferencia  ${{ number_format(  $sales->where('payment_status',1)->sum('total') - $sales->where('payment_status',1)->sum('total_cost'),0,',','.' ) }}</div>
-        
-        <div> Consumos {{ $sales->where('customer_id',65)->count() }} </div>
-        <div> Total  ${{ number_format($sales->where('customer_id',65)->sum('total_cost'),0,',','.') }}</div>
-        <div> Diferencia ${{ number_format(0 - $sales->where('customer_id',65)->sum('total_cost'),0,',','.') }}</div>
-        
-        <div> Mermas {{ $sales->where('customer_id',88)->count() }} </div>
-        <div> Total  ${{ number_format($sales->where('customer_id',88)->sum('total_cost'),0,',','.') }}</div>
-        <div> Diferencia ${{ number_format(0 - $sales->where('customer_id',88)->sum('total_cost'),0,',','.') }}</div>
+    <div class="flex justify-center">
+        <div class="grid grid-cols-3 gap-4 w-max-content">
+            <div> Ventas totales {{ $sales->count() }} </div>
+            <div> Total  ${{ number_format($sales->sum('total'),0,',','.') }}</div>
+            <div> Diferencia  ${{ number_format(  $sales->sum('total') - $sales->sum('total_cost'),0,',','.') }}</div>
+            
+            <div> Consumos {{ $sales->where('customer_id',65)->count() }} </div>
+            <div> Total  ${{ number_format($sales->where('customer_id',65)->sum('total_cost'),0,',','.') }}</div>
+            <div> Diferencia ${{ number_format(0 - $sales->where('customer_id',65)->sum('total_cost'),0,',','.') }}</div>
+            
+            <div> Mermas {{ $sales->where('customer_id',88)->count() }} </div>
+            <div> Total  ${{ number_format($sales->where('customer_id',88)->sum('total_cost'),0,',','.') }}</div>
+            <div> Diferencia ${{ number_format(0 - $sales->where('customer_id',88)->sum('total_cost'),0,',','.') }}</div>
+        </div>
+    
+        <div>
+            <div> Ventas pagadas {{ $sales->where('payment_status',3)->count() }} </div>
+            <div> Total  ${{ number_format($sales->where('payment_status',3)->sum('total'),0,',','.') }}</div>
+            <div> Diferencia  ${{ number_format( $sales->where('payment_status',3)->sum('total') - $sales->where('payment_status',3)->sum('total_cost'),0,',','.') }}</div>
+            
+            <div> Ventas pendientes {{ $sales->where('payment_status',1)->count() }} </div>
+            <div> Total  ${{ number_format($sales->where('payment_status',1)->sum('total'),0,',','.') }}</div>
+            <div> Diferencia  ${{ number_format(  $sales->where('payment_status',1)->sum('total') - $sales->where('payment_status',1)->sum('total_cost'),0,',','.' ) }}</div>
+        </div>
     </div>
+   
 
 
     <div class="py-4 flex items-center">
