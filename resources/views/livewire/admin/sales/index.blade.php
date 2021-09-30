569a2ebcffb8f02cd4dd1d50a605215c85884224
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-09-30 00:18:06 -0300

    ajuste

--- resources/views/livewire/admin/sales/index.blade.php
+++ resources/views/livewire/admin/sales/index.blade.php
@@ -28,6 +28,10 @@
         <div> Ventas pendientes {{ $sales->where('payment_status',1)->count() }} </div>
         <div> Total  ${{ number_format($sales->where('payment_status',1)->sum('total'),0,',','.') }}</div>
         <div> Diferencia  ${{ number_format(  $sales->where('payment_status',1)->sum('total') - $sales->where('payment_status',1)->sum('total_cost'),0,',','.' ) }}</div>
+        
+        <div> Consumos {{ $sales->where('customer',65)->count() }} </div>
+        <div> Total  ${{ number_format($sales->where('customer',65)->sum('total'),0,',','.') }}</div>
+        <div> Diferencia  ${{ number_format(  $sales->where('payment_status',1)->sum('total') - $sales->where('payment_status',1)->sum('total_cost'),0,',','.' ) }}</div>
     </div>
 
 
