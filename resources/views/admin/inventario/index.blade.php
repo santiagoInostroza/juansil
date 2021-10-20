ca97a39c8ee0e38823af692d89cdb8a9607a6438
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-10-19 22:22:10 -0300

    ajuste

--- resources/views/admin/inventario/index.blade.php
+++ resources/views/admin/inventario/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.admin4')
 
 @section('content')
+@php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
+@endphp 
     @livewire('admin.inventario.index')
 @endsection
