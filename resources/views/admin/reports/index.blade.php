7dd307c7f6e43477267375db2416a84ebb282985
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-10-19 22:21:33 -0300

    ajuste

--- resources/views/admin/reports/index.blade.php
+++ resources/views/admin/reports/index.blade.php
@@ -2,6 +2,12 @@
 @extends('layouts.admin4')
 
 @section('content')
+@php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
+@endphp 
     @livewire('admin.reports.index')    
 @endsection
 
