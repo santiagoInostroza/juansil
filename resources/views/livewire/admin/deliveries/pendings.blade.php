4709d622cfc425b79efb76d0ef2a129b7e53e867
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-11-16 03:48:13 -0300

    ajuste

--- resources/views/livewire/admin/deliveries/pendings.blade.php
+++ resources/views/livewire/admin/deliveries/pendings.blade.php
@@ -5,7 +5,7 @@
         </div>
     @else
         <div class="card-header">
-            <h2 class="" x-on:click="open=!open" style="cursor: pointer">Pendientes ({{ $entregas_pendientes }})</h2>
+            <h2 class="" x-on:click="open=!open" style="cursor: pointer">Pendientes ({{ $entregas_pendientes }} {{$venta->sum('total')}})</h2>
         </div>
 
         <div class="hidden" :class="{'hidden':!open}" >
