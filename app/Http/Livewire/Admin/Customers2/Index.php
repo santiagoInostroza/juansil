c60c5b7d548ef8f4fd0084f81c58160ce8bf8bdb
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-11-29 22:15:40 -0300

    ajuste

--- app/Http/Livewire/Admin/Customers2/Index.php
+++ app/Http/Livewire/Admin/Customers2/Index.php
@@ -9,13 +9,13 @@ use App\Models\Customer;
 
 class Index extends Component{
 
-    public $nombreComuna = "la granja";
+    public $nombreComuna = "";
     
     public function render(){
         $comunas = Comuna::where('tiene_reparto',1)->get();
         $customers = Customer::with('sales')
         ->orderBy('comuna', 'asc')
-        ->where('comuna', $this->nombreComuna)
+        ->where('comuna','like', '%'. $this->nombreComuna . '%')
         ->get();
         $users = User::all();
         return view('livewire.admin.customers2.index',compact('customers','users','comunas') );
