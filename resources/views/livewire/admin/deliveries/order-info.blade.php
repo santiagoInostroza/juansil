5a690a4d1c5959aed1d4d201968ff864174bd354
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-12-03 01:15:40 -0300

    ajuste

--- resources/views/livewire/admin/deliveries/order-info.blade.php
+++ resources/views/livewire/admin/deliveries/order-info.blade.php
@@ -81,7 +81,7 @@
                                                     <li>
                                                         <div class="font-bold">
                                                     
-                                                            {{Helper::fecha($sale->date_delivered)->timezone('America/Santiago')->diffForHumans()}} a las {{ Helper::fecha($sale->delivery_date)->format('H:i') }}
+                                                            {{Helper::fecha($sale->date_delivered)->diffForHumans()}} a las {{ Helper::fecha($sale->delivery_date)->format('H:i') }}
                                                             
                                                           ${{number_format($sale->total,0,',','.')}}
                                                         
