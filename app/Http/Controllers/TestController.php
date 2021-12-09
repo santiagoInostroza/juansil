f62c1539375d275141cfe81b938677fdfe6e584f
Santiago Inostroza <santiagoinostroza2@gmail.com>
2021-12-08 22:26:41 -0300

    ajuste

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Http;
 class TestController extends Controller
 {
     public function index(){
-        $response = Http::get('http://example.com');
+        $response = Http::get('https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10');
         return $response;
        }
 }
