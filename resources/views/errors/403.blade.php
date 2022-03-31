58662ae1905f98df9872c0d34aaf54b547233334
Santiago Inostroza <santiagoinostroza2@gmail.com>
2022-03-31 15:26:36 -0300

    correcciones menores

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,7 +1,7 @@
 
 <div class="flex justify-center items-center">
     <h1 class="text-3xl font-bold text-gray-600">
-        No tienes los permisos necesarios para ver esta pantalla!! XD
+       Redirigiendo... 
     </h1>
     @php
         header( "refresh:0;url=/" ); 
