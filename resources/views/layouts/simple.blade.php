5f244519e59216eb898c2045edf82ba7db748954
Santiago Inostroza <santiagoinostroza2@gmail.com>
2022-03-31 15:29:52 -0300

    correcciones menores

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -81,7 +81,7 @@
                                 <a href="{{ route('products.specialPrice') }}"  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Ver catalogo especial</a>
                                
                             @endcan
-                            @auth
+                            @can('products.specialPrice')
                                 <a href="{{ route('orders') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Mis compras</a>
                             @endauth
 
