f7b7d744b3a42311ef7a267dc14166eee7fba335
Santiago Inostroza <santiagoinostroza2@gmail.com>
2022-03-31 22:42:05 -0300

    correcciones menores

--- app/Http/Controllers/Admin2/DashboardController.php
+++ app/Http/Controllers/Admin2/DashboardController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers\Admin2;
 
-use App\Http\Controllers\Controller;
+use App\Models\Sale;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class DashboardController extends Controller{
 
     public function index(){
         $month = 3;
+        //sales distinct user_created
+        $sales = Sale::distinct('user_created')->get('user_created as user')->pluck('user');
+        $users = User::whereIn('id',$sales)->get();
 
         return view('admin2.dashboard.index',[
-            'users' => \App\Models\User::all(),
+            'users' => $users,
             'month' => $month,
         ]);
      }
