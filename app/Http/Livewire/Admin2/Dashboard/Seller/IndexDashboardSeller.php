6cf6122580eadd4c41f32fa860256ce3c1dd341b
Santiago Inostroza <santiagoinostroza2@gmail.com>
2022-03-31 22:23:30 -0300

    correcciones menores

--- app/Http/Livewire/Admin2/Dashboard/Seller/IndexDashboardSeller.php
+++ app/Http/Livewire/Admin2/Dashboard/Seller/IndexDashboardSeller.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Admin2\Dashboard\Seller;
 use Livewire\Component;
 
 class IndexDashboardSeller extends Component{
+    public $user;
     public $month;
     public $salesOfTheYearCompleted;
     public $salesOfTheMonthCompleted;
@@ -23,11 +24,12 @@ class IndexDashboardSeller extends Component{
 
     public function render(){
         
-        $this->salesOfTheYearCompleted = auth()->user()->salesOfTheYearCompleted();
-        $this->salesOfToday = auth()->user()->salesOfToday();
+        $this->salesOfTheYearCompleted = $this->user->salesOfTheYearCompleted();
+        $this->salesOfToday = $this->user->salesOfToday();
 
 
-        $this->salesOfTheMonthCompleted = auth()->user()->salesOfTheMonthCompleted($this->month);
+        $this->salesOfTheMonthCompleted =  $this->user->salesOfTheMonthCompleted($this->month);
+
         $this->salesLessThan30k =  $this->salesOfTheMonthCompleted->where('total','<',30000)->count();
         $this->salesgreaterThan30k =  $this->salesOfTheMonthCompleted->where('total','>=',30000)->where('total','<',50000)->count();
         $this->salesgreaterThan50k =  $this->salesOfTheMonthCompleted->where('total','>=',50000)->count();
@@ -35,10 +37,6 @@ class IndexDashboardSeller extends Component{
         $this->totalToPay = ($this->salesLessThan30k * 400) + ($this->salesgreaterThan30k * 600) + ($this->salesgreaterThan50k * 800);
         
 
-        return view('livewire.admin2.dashboard.seller.index-dashboard-seller',[
-            'salesOfTheYearCompleted' => auth()->user()->salesOfTheYearCompleted(),
-            'salesOfTheMonthCompleted' => auth()->user()->salesOfTheMonthCompleted($this->month),
-            'salesOfToday' => auth()->user()->salesOfToday(),
-        ]);
+        return view('livewire.admin2.dashboard.seller.index-dashboard-seller');
     }
 }
